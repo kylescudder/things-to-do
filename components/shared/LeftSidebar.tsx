cefdeb8e02e91f189e09d24fcb71c7d0c5344fb5
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-08-28 19:56:52 +0100

    Tailwindcss adjustments

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -15,20 +15,18 @@ export default function LeftSidebar(props: {
   return (
     <section
       className="custom-scrollbar 
-		leftsidebar"
+		  sticky left-0 top-0 z-20 h-screen w-fit justify-between 
+      overflow-auto border-r border-r-dark-4 bg-light-2 
+    dark:bg-dark-2 pt-28 max-md:hidden flex flex-col pb-0"
     >
-      <div
-        className="flex 
-			w-full flex-1
-			flex-col gap-3 px-6"
-      >
+      <div className="flex-1 flex flex-col gap-3 px-6 overflow-y-auto">
         <NavOptions
           func={pullData}
           position="leftsidebar"
           categories={props.categories}
         />
       </div>
-      <div className="mt-10 px-6">
+      <div className="mt-auto px-6 py-3 shadow-inner">
         <Logout placement="" />
       </div>
     </section>
