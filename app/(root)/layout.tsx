58532915f224720c02fba6c240f962bb73c52a55
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-09-05 15:37:13 +0100

    Added metadata icons

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -22,6 +22,34 @@ export const metadata: Metadata = {
   applicationName: "Things To Do",
   manifest: "/manifest.json",
   themeColor: "#877EFF",
+  icons: [
+    {
+      url: "/assets/maskable_icon_x48",
+      type: "image/png",
+      sizes: "48x48",
+    },
+    {
+      url: "/assets/maskable_icon_x72",
+      type: "image/png",
+      sizes: "72x72",
+    },
+    {
+      url: "/assets/maskable_icon_x96.png",
+      type: "image/png",
+      sizes: "96x96",
+    },
+    {
+      url: "/assets/maskable_icon_x128.png",
+      type: "image/png",
+      sizes: "128x128",
+    },
+    {
+      url: "/assets/maskable_icon_x192.png",
+      type: "image/png",
+      sizes: "192x192",
+      rel: "apple-touch-icon",
+    },
+  ],
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
