f8cbefc1f97c25f153f77e7bee8aeaafb1bdeb9c
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-09-05 15:42:38 +0100

    Added meta data to auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,6 +10,62 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Things To Do",
   description: "All your To Dos, organised, simplified, accessible...",
+  applicationName: "Things To Do",
+  manifest: "/manifest.json",
+  themeColor: "#877EFF",
+  icons: [
+    {
+      url: "/assets/maskable_icon_x48",
+      type: "image/png",
+      sizes: "48x48",
+    },
+    {
+      url: "/assets/maskable_icon_x72",
+      type: "image/png",
+      sizes: "72x72",
+    },
+    {
+      url: "/assets/maskable_icon_x96.png",
+      type: "image/png",
+      sizes: "96x96",
+    },
+    {
+      url: "/assets/maskable_icon_x128.png",
+      type: "image/png",
+      sizes: "128x128",
+    },
+    {
+      url: "/assets/maskable_icon_x192.png",
+      type: "image/png",
+      sizes: "192x192",
+      rel: "apple-touch-icon",
+    },
+  ],
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Things To Do",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Things To Do",
+    title: {
+      default: "Things To Do",
+      template: "% - PWA App",
+    },
+    description: "All your To Dos, organised, simplified, accessible...",
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: "Things To Do",
+      template: "% - PWA App",
+    },
+    description: "All your To Dos, organised, simplified, accessible...",
+  },
 };
 
 export default function RootLayout({
